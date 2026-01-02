340967765879e680f29a5711cfe1c2fb225b919b
foysalthecode <ahmedmdfoysal123@gmail.com>
2026-01-02 17:37:57 +0600

    update operation

--- crud.ts
+++ crud.ts
@@ -2,18 +2,14 @@ import { prisma } from "./lib/prisma";
 
 async function run() {
   //* create user -----------------------------------------------------------
-
   //   const createUser = await prisma.user.create({
   //     data: {
   //       name: "Mahi",
   //       email: "Mahi@gmail.com",
   //     },
   //   });
-
   //   console.log("created user :", createUser);
-
   //* create post -----------------------------------------------------------
-
   //   const createPost = await prisma.post.create({
   //     data: {
   //       title: "this is a title",
@@ -21,37 +17,54 @@ async function run() {
   //       authorId: 1,
   //     },
   //   });
-
   //   console.log("Created Post :", createPost);
-
   //* create profile -----------------------------------------------------------
-
   //   const createProfile = await prisma.profile.create({
   //     data: {
   //       bio: "Next Level Development - L2",
   //       userId: 1,
   //     },
   //   });
-
   //   console.log("Created Profile :", createProfile);
-
   //* Find all user  -----------------------------------------------------------
-  const users = await prisma.user.findMany({
-    // include: {           // include diye main data er sathe included data gula add hoye jay
-    //   posts: true,
-    //   profiles: true,
-    // },
+  //   const users = await prisma.user.findMany({
+  // include: {           // include diye main data er sathe included data gula add hoye jay
+  //   posts: true,
+  //   profiles: true,
+  // },
+  //     select: {
+  // select diye only selected item gula e dekha jay
+  //       id: true,
+  //       name: true,
+  //       email: true,
+  //       posts: true,
+  //       profiles: true,
+  //     },
+  //   });
+  //   console.dir(users, { depth: Infinity });
+  //* Update user -----------------------------------------------------------
 
+  const updateUser = await prisma.profile.update({
+    where: {
+      userId: 1,
+    },
+    data: {
+      bio: "Web Developer and Student",
+      dateOfBirth: "2005-06-13T10:50:07.368Z",
+    },
     select: {
-      // select diye only selected item gula e dekha jay
       id: true,
-      name: true,
-      email: true,
-      posts: true,
-      profiles: true,
+      bio: true,
+      user: {
+        select: {
+          name: true,
+          email: true,
+        },
+      },
     },
   });
-  console.dir(users, { depth: Infinity });
+
+  console.log(updateUser);
 }
 
 run();
